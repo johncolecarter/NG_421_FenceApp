701c8b26fe164ffe6c98b53b5655ab5fa29ad80e
johncolecarter <colecarter7777@gmail.com>
2020-04-30 13:34:19 -0500

    added logout

--- FenceApp/src/app/user-profile/user-profile.component.ts
+++ FenceApp/src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,16 +11,41 @@ export class UserProfileComponent implements OnInit {
 
   userProfileForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
     this.userProfileForm = this.formBuilder.group({
       inputFirstName: ['', Validators.compose([
         Validators.required,
         Validators.minLength(2)
-      ])]
+      ])],
+      inputLastName: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)
+      ])],
+      inputBirthday: ['', Validators.compose([
+        Validators.required
+      ])],
+      inputAddress: ['', Validators.compose([
+        Validators.required
+      ])],
+      inputFood: [''],
+      inputMovie: [''],
+      inputMusic: [''],
+      inputHobbies: ['']
     });
   }
 
   ngOnInit(): void {
+  }
+
+  save() {
+    console.log(this.userProfileForm.value);
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
   }
 
 }
