a79350fc38f7cec6f8992a7b5f1e210e6cd78536
johncolecarter <colecarter7777@gmail.com>
2020-04-30 11:37:15 -0500

    added register

--- FenceApp/src/app/register/register.component.ts
+++ FenceApp/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      inputEmail: ['', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      inputFirstName: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)
+      ])],
+      inputLastName: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)
+      ])],
+      inputPassword: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(7)
+      ])],
+      inputConfirmPassword: ['', Validators.compose([
+        Validators.required,
+      ])]
+    });
+  }
 
   ngOnInit(): void {
   }
